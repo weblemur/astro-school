2b7768ed8f012a851938b0c4926343985a83f035
Martin Gutfeldt <martingutfeldt@gmail.com>
2017-08-30 14:09:33 -0500

    Complete basic model validations

--- db/models/student.js
+++ db/models/student.js
@@ -4,6 +4,15 @@ var db = require('../db.js');
 
 
 module.exports = db.define('student', {
-  name: Sequelize.STRING,
-  email: Sequelize.STRING,
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  email: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      isEmail: true
+    }
+  }
 });
--- specs/models.spec.js
+++ specs/models.spec.js
@@ -23,3 +23,56 @@ describe('Campus Model', () => {
     });
   });
 });
+
+describe('Student Model', () => {
+  let student;
+  beforeEach(() => student = Student.build());
+
+  describe('Validations', () => {
+    it('errors without a name', () => {
+      return student.validate()
+        .then(() => {
+          throw new Error('validation should fail when name is null');
+        },
+        err => {
+          expect(err).to.be.an('error');
+          expect(err.errors).to.contain.a.thing.with.property('path', 'name');
+        });
+    });
+    it('errors without an email', () => {
+      return student.validate()
+        .then(() => {
+          throw new Error('validation should fail when email is null');
+        },
+        err => {
+          expect(err).to.be.an('error');
+          expect(err.errors).to.contain.a.thing.with.property('path', 'email');
+        });
+    });
+    it('errors with an invalid email', () => {
+      student.name = 'name';
+      student.email = 'email';
+      student.campusId = 1;
+      return student.validate()
+        .then(() => {
+          throw new Error('validation should fail when email is invalid');
+        },
+        err => {
+          expect(err).to.be.an('error');
+          expect(err.errors).to.contain.a.thing.with.property('path', 'email');
+        });
+    });
+    it('errors without an assigned campus', () => {
+      student.name = 'name';
+      student.email = 'email@domain.com';
+      return student.validate()
+        .then(() => {
+          throw new Error('validation should fail when not assigned to a campus');
+        },
+        err => {
+          expect(err).to.be.an('error');
+          expect(err.errors).to.contain.a.thing.with.property('path', 'campusId');
+        });
+    });
+  });
+});
