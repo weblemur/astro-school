1893174ddd2bea11ce1b9d7886b17e3849bcefc7
Martin Gutfeldt <martingutfeldt@gmail.com>
2017-08-30 15:43:24 -0500

    Complete restful routes for the campus API

--- server/api/campuses.js
+++ server/api/campuses.js
@@ -1,10 +1,51 @@
-const campusesApi = require('express').Router();
+const campusApi = require('express').Router();
 const { Campus } = require('../../db/models');
 
-campusesApi.get('/', (req, res, next) => {
+campusApi.param('id', (req, res, next, id) => {
+  Campus.findById(id)
+    .then(campus => {
+      if (!campus) {
+        res.sendStatus(404);
+      } else {
+        req.campus = campus;
+        next();
+      }
+    })
+    .catch(next);
+});
+
+campusApi.get('/', (req, res, next) => {
   Campus.findAll()
     .then(campuses => res.json(campuses))
     .catch(next);
 });
 
-module.exports = campusesApi;
+campusApi.get('/:id', (req, res, next) => {
+  res.json(req.campus);
+});
+
+campusApi.post('/', (req, res, next) => {
+  Campus.create(req.body)
+    .then(campus => {
+      res.status(201).json(campus);
+    })
+    .catch(next);
+});
+
+campusApi.put('/:id', (req, res, next) => {
+  req.campus.update(req.body)
+    .then(campus => {
+      res.status(200).json(campus);
+    })
+    .catch(next);
+});
+
+campusApi.delete('/:id', (req, res, next) => {
+  req.campus.destroy()
+    .then(() => {
+      res.sendStatus(204);
+    })
+    .catch(next);
+});
+
+module.exports = campusApi;
--- specs/api.spec.js
+++ specs/api.spec.js
@@ -14,7 +14,7 @@ const testCampuses = [
     ] }
 ];
 
-before(() => {
+beforeEach(() => {
   return db.sync({force: true})
     .then(() => testCampuses)
     .map(campus => Campus.create(campus, { include: [Student] }));
@@ -33,6 +33,91 @@ describe('Campus API routes', () => {
         .then(res => {
           expect(res.body.length).to.equal(2);
         });
+    });
+  });
+
+  describe('GET /api/campuses/:id', () => {
+    it('sends info for a specific campus', () => {
+      return agent.get('/api/campuses/1')
+        .expect(200)
+        .expect('Content-Type', /json/)
+        .then(res => {
+          expect(res.body.id).to.equal(1);
+        });
+    });
+    it('404s for non-existent campus', () => {
+      return agent.get('/api/campuses/4')
+        .expect(404);
+    });
+  });
+
+  describe('POST /api/campuses/', () => {
+    it('creates a new campus', () => {
+      return agent.post('/api/campuses/')
+        .send({ name: 'New Campus', imageUrl: 'test.jpg' })
+        .expect(201)
+        .expect('Content-Type', /json/)
+        .then(res => {
+          expect(res.body.id).to.be.a('number');
+          expect(res.body.name).to.equal('New Campus');
+        })
+        .then(() => Campus.findAll())
+        .then(campuses => {
+          expect(campuses.length).to.equal(3);
+        });
+    });
+    it('500s for an invalid campus', () => {
+      return agent.post('/api/campuses/')
+        .send({})
+        .expect(500)
+        .then(() => Campus.findAll())
+        .then(campuses => {
+          expect(campuses.length).to.equal(2);
+        });
+    });
+  });
+
+  describe('PUT /api/campuses/:id', () => {
+    it('updates a specific campus', () => {
+      return agent.put('/api/campuses/1')
+        .send({name: 'Updated Campus'})
+        .expect(200)
+        .expect('Content-Type', /json/)
+        .then(res => {
+          expect(res.body.name).to.equal('Updated Campus');
+        })
+        .then(() => Campus.findById(1))
+        .then(campus => {
+          expect(campus.name).to.equal('Updated Campus');
+        });
+    });
+    it('404s for non-existent campus', () => {
+      return agent.put('/api/campuses/4')
+        .expect(404);
+    });
+    it('500s for invalid updates', () => {
+      return agent.put('/api/campuses/1')
+        .send({name: null})
+        .expect(500)
+        .then(() => Campus.findById(1))
+        .then(campus => {
+          expect(campus.name).to.equal('First Campus');
+        });
+    });
+  });
+
+  describe('DELETE /api/campuses/:id', () => {
+    it('deletes a specific campus', () => {
+      return agent.delete('/api/campuses/1')
+        .expect(204)
+        .then(() => Campus.findById(1))
+        .then(campus => {
+          expect(campus).to.equal(null);
+        });
+    });
+    it('404s for non-existent campus', () => {
+      return agent.delete('/api/campuses/4')
+        .expect(404);
     });
   });
 });
