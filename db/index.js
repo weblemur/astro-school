b6ba64dda06cdaeb41f5d5844c12b72c19fbbec9
Martin Gutfeldt <martingutfeldt@gmail.com>
2017-08-30 09:04:01 -0500

    Syntax cleanup - db/index.js

--- db/index.js
+++ db/index.js
@@ -1,4 +1,4 @@
-'use strict'
+'use strict';
 const debug = require('debug')('sql');
 const chalk = require('chalk');
 const Sequelize = require('sequelize');
@@ -16,7 +16,7 @@ const db = module.exports = new Sequelize(connectionString, {
 });
 
 // run our models file (makes all associations for our Sequelize objects)
-require('./models')
+require('./models');
 
 // sync the db, creating it if necessary
 function sync(force=false, retries=0, maxRetries=5) {
@@ -26,19 +26,19 @@ function sync(force=false, retries=0, maxRetries=5) {
     // Don't do this auto-create nonsense in prod, or
     // if we've retried too many times. 
     if (process.env.NODE_ENV === 'production' || retries > maxRetries) {
-      console.error(chalk.red(`********** database error ***********`))
-      console.error(chalk.red(`    Couldn't connect to ${connectionString}`))
-      console.error()
-      console.error(chalk.red(fail))
-      console.error(chalk.red(`*************************************`))
-      return
+      console.error(chalk.red(`********** database error ***********`));
+      console.error(chalk.red(`    Couldn't connect to ${connectionString}`));
+      console.error();
+      console.error(chalk.red(fail));
+      console.error(chalk.red(`*************************************`));
+      return;
     }
     // Otherwise, do this autocreate nonsense
-    console.log(`${retries ? `[retry ${retries}]` : ''} Creating database ${name}...`)
+    console.log(`${retries ? `[retry ${retries}]` : ''} Creating database ${name}...`);
     return new Promise((resolve, reject) =>
       require('child_process').exec(`createdb "${name}"`, resolve)
-    ).then(() => sync(true, retries + 1))
-  })
+    ).then(() => sync(true, retries + 1));
+  });
 }
 
 db.didSync = sync();
