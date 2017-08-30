189072157aaf65dbfc27dd9d9f6bd7372a3999c9
Martin Gutfeldt <martingutfeldt@gmail.com>
2017-08-30 13:55:58 -0500

    Add validations to pass starting test

--- db/models/campus.js
+++ db/models/campus.js
@@ -2,8 +2,13 @@
 var Sequelize = require('sequelize');
 var db = require('../db.js');
 
-
 module.exports = db.define('campus', {
-  name: Sequelize.STRING,
-  imageUrl: Sequelize.STRING,
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  imageUrl: {
+    type: Sequelize.STRING,
+    allowNull: false
+  }
 });
