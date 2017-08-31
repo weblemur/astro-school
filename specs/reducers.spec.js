c80c6c3bf61438ccc6a62fdc77a71503803c664a
Martin Gutfeldt <martingutfeldt@gmail.com>
2017-08-31 09:57:17 -0500

    Scaffold specs for campus reducer

---
+++ specs/reducers.spec.js
@@ -0,0 +1,23 @@
+const { expect } = require('chai').use(require('chai-things'));
+const { createStore, applyMiddleware } = require('redux');
+const reducers = require('../app/reducers');
+const thunkMiddleware = require('redux-thunk');
+let store = {};
+
+beforeEach(() => store = createStore(reducers, applyMiddleware(thunkMiddleware)));
+
+describe('Campus Store', () => {
+  it('is an array');
+  describe('actions:', () => {
+    it('can initialize the whole array');
+    it('can add a campus');
+    it('can remove a campus');
+    it('can edit a campus');
+  });
+  describe('thunks:', () => {
+    it('can fetch all the campuses');
+    it('can create a campus');
+    it('can delete a campus');
+    it('can update a campus');
+  });
+});
