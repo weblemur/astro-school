53146cf29b2e996b509deb0bdfc20a3cb7577de6
Martin Gutfeldt <martingutfeldt@gmail.com>
2017-08-31 13:18:36 -0500

    Complete basic campus reducer w/ actions

--- app/reducers/campuses.jsx
+++ app/reducers/campuses.jsx
@@ -1,5 +1,35 @@
-export default function reducer (state = [], action) {
+/***     ACTIONS      ***/
+
+const INIT_CAMPUSES = 'INIT_CAMPUSES';
+const ADD_CAMPUS = 'ADD_CAMPUS';
+const REMOVE_CAMPUS = 'REMOVE_CAMPUS';
+const EDIT_CAMPUS = 'EDIT_CAMPUS';
+
+/*** ACTIONS CREATORS ***/
+
+export const initCampuses = (campuses) => ({ type: INIT_CAMPUSES, campuses });
+export const addCampus = (campus) => ({ type: ADD_CAMPUS, campus });
+export const removeCampus = (campus) => ({ type: REMOVE_CAMPUS, campus });
+export const editCampus = (campus) => ({ type: EDIT_CAMPUS, campus });
+
+/***     THUNKS       ***/
+
+/***     REDUCER      ***/
+
+export default (state = [], action) => {
   switch (action.type) {
+    case INIT_CAMPUSES:
+      return action.campuses;
+
+    case ADD_CAMPUS:
+      return state.concat(action.campus);
+
+    case REMOVE_CAMPUS:
+      return state.filter(campus => campus.id !== action.campus.id);
+
+    case EDIT_CAMPUS:
+      return state.map(campus => campus.id !== action.campus.id ? campus : action.campus);
+
     default: return state;
   }
-}
+};
--- specs/reducers.spec.js
+++ specs/reducers.spec.js
@@ -1,7 +1,7 @@
 import chai from 'chai';
 import chaiThings from 'chai-things';
 import { createStore, applyMiddleware } from 'redux';
-import campusReducer from '../app/reducers/campuses';
+import campusReducer, { initCampuses, addCampus, removeCampus, editCampus } from '../app/reducers/campuses';
 import thunkMiddleware from 'redux-thunk';
 
 const expect = chai.use(chaiThings).expect;
@@ -11,16 +11,33 @@ describe('Reducer Specs ––', () => {
   describe('Campus Store', () => {
     beforeEach(() => {
       store = createStore(campusReducer, applyMiddleware(thunkMiddleware));
-      store = createStore(campusReducer);
+      Object.freeze(store.getState());
     });
     it('is an array', () => {
       expect(store.getState()).to.be.an('array');
+      expect(store.getState().length).to.equal(0);
     });
     describe('actions:', () => {
-      it('can initialize the whole array');
-      it('can add a campus');
-      it('can remove a campus');
-      it('can edit a campus');
+      it('can initialize the whole array', () => {
+        store.dispatch(initCampuses([{ id: 1 }, { id: 2 }]));
+        expect(store.getState().length).to.equal(2);
+      });
+      it('can add a campus', () => {
+        store.dispatch(addCampus({ id: 1 }));
+        store.dispatch(addCampus({ id: 2 }));
+        expect(store.getState().length).to.equal(2);
+      });
+      it('can remove a campus', () => {
+        store.dispatch(initCampuses([{ id: 1 }, { id: 2 }]));
+        store.dispatch(removeCampus({ id: 1 }));
+        expect(store.getState().length).to.equal(1);
+        expect(store.getState()[0]).to.have.property('id', 2);
+      });
+      it('can edit a campus', () => {
+        store.dispatch(initCampuses([{ id: 1 }, { id: 2 }]));
+        store.dispatch(editCampus({ id: 2, name: 'updated' }));
+        expect(store.getState()[1]).to.have.property('name', 'updated');
+      });
     });
     describe('thunks:', () => {
       it('can fetch all the campuses');
