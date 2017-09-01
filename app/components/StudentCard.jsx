e219fc0696ccc8747a68f358d30685f1201cf2dd
Martin Gutfeldt <martingutfeldt@gmail.com>
2017-09-01 09:13:43 -0500

    Rework code for campus select in student list

--- app/components/StudentCard.jsx
+++ app/components/StudentCard.jsx
@@ -3,22 +3,19 @@ import { Link } from 'react-router-dom';
 
 export default function StudentCard (props) {
   const { id, name, email, campusId } = props.student;
-  const campuses = props.campuses;
+  const campuses = props.campuses || [];
   return (
     <li className="student_info">
       <Link to={`/students/${id}`}>
         <h2 className="student_name">{name}</h2>
       </Link>
       <p className="student_email">{email}</p>
-      { campuses ?
         <div className="student_campus">
           <label htmlFor={`${name}_campus`}>Campus:</label>
           <select id={`${name}_campus`} className="student_campus_select" value={campusId}>
             { campuses.map(campus => <option value={campus.id}>{campus.name}</option>) }
           </select>
         </div>
-        : null
-      }
     </li>
   );
 }
