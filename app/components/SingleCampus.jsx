a68bb64aecbe92ee492df314d87cd2ff291addeb
Martin Gutfeldt <martingutfeldt@gmail.com>
2017-09-01 11:29:09 -0500

    Make single campus view stateful

--- app/components/SingleCampus.jsx
+++ app/components/SingleCampus.jsx
@@ -1,34 +1,53 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import StudentCard from './StudentCard';
 
-const SingleCampus = (props) => {
-  const { campus, students } = props;
-  const { name, imageUrl } = campus;
+class SingleCampus extends Component {
+  constructor (props) {
+    super(props);
+    console.log(props);
+    this.state = {
+      id: Number(props.match.params.id),
+      campuses: [],
+      currentCampus: {},
+      students: [],
+      currentStudents: []
+    };
+  }
 
-  return (
-    <div id="content">
-      <header className="page_header">
-        <h1 className="page_title">{name}</h1>
-        <button className="btn edit_btn add_campus_button">Edit Campus</button>
-      </header>
-      <div className="campus_full">
-        <div className="planet">
-          <img src={imageUrl} alt={`${name} image`} />
+  componentWillReceiveProps (newProps) {
+    if (newProps.campuses && newProps.campuses.length > 0) {
+      const currentCampus = newProps.campuses.find(campus => campus.id === this.state.id) || {};
+      this.setState({ campuses: newProps.campuses, currentCampus });
+    }
+
+    if (newProps.students && newProps.students.length > 0) {
+      const currentStudents = newProps.students.filter(student => student.campusId === this.state.id) || [];
+      this.setState({ students: newProps.students, currentStudents });
+    }
+  }
+
+  render () {
+    const { currentCampus, currentStudents, campuses } = this.state;
+    return (
+      <div id="content">
+        <header className="page_header">
+          <h1 className="page_title">{currentCampus.name}</h1>
+          <button className="btn edit_btn add_campus_button">Edit Campus</button>
+        </header>
+        <div className="campus_full">
+          <div className="planet">
+            <img src={currentCampus.imageUrl} alt={`${currentCampus.name} image`} />
+          </div>
+          <ul className="campus_list">
+            { currentStudents.map(student => <StudentCard key={student.id} student={student} campuses={campuses} />)}
+          </ul>
         </div>
-        <ul className="campus_list">
-          { students.map(student => <StudentCard key={student.id} student={student} campuses={props.campuses} />)}
-        </ul>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
-const mapState = (state, ownProps) => {
-  const thisId = Number(ownProps.match.params.id);
-  const thisCampus = state.campuses.find(campus => campus.id === thisId);
-  const students = state.students.filter(student => student.campusId === thisId);
-  return { campus: thisCampus || {}, students };
-};
+const mapState = ({ campuses, students }) => ({ campuses, students });
 
 export default connect(mapState)(SingleCampus);
